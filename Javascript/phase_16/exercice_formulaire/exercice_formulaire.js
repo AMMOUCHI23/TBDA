9b7ef27ab9de7bd03636ac92efe70a2aa88595d2
AMMOUCHI Abdallah <bily-f@hotmail.fr>
2023-05-15 15:54:26 +0200

    modification sur le fichier js de phase 16

--- Javascript/phase_16/exercice_formulaire/exercice_formulaire.js
+++ Javascript/phase_16/exercice_formulaire/exercice_formulaire.js
@@ -6,20 +6,20 @@ var ville = document.getElementById("ville").value;
 var email = document.getElementById("email").value;
 var choix= document.getElementById("choix").value;
 var projet= document.getElementById("projet").value;
-
+// Fonction qui recupere l'élément choisi dans select pour le mettre dans la zone texte
 function change(){
 var selectElmt = document.getElementById("choix");
 var valeurselectionnee = selectElmt.options[selectElmt.selectedIndex].value;
 var textselectionne = selectElmt.options[selectElmt.selectedIndex].text;
+if(textselectionne=="Choisissez"){
+    document.getElementById("projet").value=""
+}else{
 document.getElementById("projet").value=textselectionne;
 }
-
-//document.getElementById("choix").innerHTML=textselectionne;
-
+}
+// Fonction qui teste le formulaire avant de lenvoyer
 document.getElementById("forme1").addEventListener("submit", function (f) {
     f.preventDefault();
- 
-    //var choix=selectBox[selectBox.selecteIndex].value;
     
     
     if (societe == "") {
